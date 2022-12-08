6909d2e5096fecb63553f2de8fdc6a549e06707d
minhaj-boop <imminhajj@gmail.com>
2022-12-08 23:53:46 +0600

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  "imagebaseurl": "",
+  "name": "Md. Jubair Ahmed Sourov",
+  "role": "Frontend Developer and Data Scientist",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/rbhatia46",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/rbhatia46",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "Passionate engineer, always motivated to learn new skills. I like to brainstorm and solve problems not only research but also for industry. As a robotics engineer, I have proficiency in hardware ranging from microcontroller-based (Arduino) to microprocessor-based (Raspberry Pi) and also software including MATLAB and python.",
+  "address": "Bangladesh",
+  "website": "Webisite: TODO",
+  "education": [
+    {
+      "UniversityName": "The LNM Insitute of Information Technology",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2020",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "UniversityName": "Some University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
