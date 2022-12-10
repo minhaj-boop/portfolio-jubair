e6455492e55b931f87b88511cb673c22a718ae2d
minhaj-boop <imminhajj@gmail.com>
2022-12-10 21:40:05 +0600

    many changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -22,13 +22,10 @@ export default class Resume extends Component {
                         {item.specialization}
                         <span>&bull;</span> <em className="date">{item.YearOfPassing}</em></p>
                       <p>
-                        <span><b>Address: </b></span>{item.address}
+                        <span><b>CGPA: </b></span>{item.finalGrade}
                         <br />
-                        <span><b>Final grade: </b></span>{item.finalGrade}
+                        <span><b>CGPA of last 2 years: </b></span>{item.cgpaLast2Years}
                         <br />
-                        <span><b>Thesis: </b></span>{item.thesis}
-                        <br />
-                        <span><b>Skills gained: </b></span>{item.skills}
                       </p>
                     </div>
                   </div>
@@ -37,31 +34,236 @@ export default class Resume extends Component {
             }
           </div>
         </div>
+
         <div className="row work">
           <div className="three columns header-col">
-            <h1><span>Work Experience</span></h1>
+            <h1><span>Research Interests</span></h1>
           </div>
 
           <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                <p>
+                  {resumeData.researchInterest}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1><span>Research Experience</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+
+            <div className="row item">
+              <div className="twelve columns">
+                <h3>{resumeData.researchExperienceInfo.name}</h3>
+                <p>
+                  <span className="info">{resumeData.researchExperienceInfo.mainDesc}</span> <span>&bull;</span> <em className="date">{resumeData.researchExperienceInfo.date}</em>
+                  <br />
+                  {resumeData.researchExperienceInfo.address}
+                </p>
+              </div>
+            </div>
+
+            <h5>Ongoing Projects</h5>
             {
-              resumeData.work && resumeData.work.map((item) => {
+              resumeData.researchExperienceProjects && resumeData.researchExperienceProjects.map((item) => {
                 return (
                   <div className="row item">
                     <div className="twelve columns">
-                      <h3>{item.CompanyName}</h3>
-                      <p className="info">
-                        {item.specialization}
-                        <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
                       <p>
-                        {item.Achievements}
+                        <span><b>{item.project}: </b></span>
+                        {item.desc}
                       </p>
                     </div>
 
                   </div>
-
                 )
               })
             }
+          </div>
+        </div>
+
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1><span>Research Interests</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                <div className="row item">
+                  <div className="twelve columns">
+                    <p>
+                      <span><b>Programming Language: </b></span>
+                      {resumeData.keySkills.programmingLang}
+                      <br />
+                      <span><b>Research Tool: </b></span>
+                      {resumeData.keySkills.researchTool}
+                      <br />
+                      <span><b>Simulation: </b></span>
+                      {resumeData.keySkills.simulation}
+                      <br />
+                      <span><b>Embadded System: </b></span>
+                      {resumeData.keySkills.embaddedSystem}
+                      <br />
+                      <span><b>Machine Operation: </b></span>
+                      {resumeData.keySkills.machineOperation}
+                      <br />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1><span>Research Publications</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {
+              resumeData.researchPublications && resumeData.researchPublications.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <p>
+                        <span><b>{item.name}</b></span>{item.desc}
+                        <br />
+                        <a href={item.link} target="_blank">{item.link}</a>
+                      </p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1><span>Research Projects</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                <div className="row item">
+                  <div className="twelve columns">
+                    <p>
+                      <span><b>{resumeData.rp1.name}</b></span>
+                      <br />
+                      &diams; {resumeData.rp1.d1}
+                      <br />
+                      &diams; {resumeData.rp1.d2}
+                      <br />
+                      &diams; {resumeData.rp1.d3}
+                      <br />
+                      &diams; {resumeData.rp1.d4}
+                      <br />
+                    </p>
+                    <p>
+                      <span><b>{resumeData.rp2.name}</b></span>
+                      <br />
+                      &diams; {resumeData.rp2.d1}
+                      <br />
+                      &diams; {resumeData.rp2.d2}
+                      <br />
+                      &diams; {resumeData.rp2.d3}
+                      <br />
+                    </p>
+                    <p>
+                      <span><b>{resumeData.rp3.name}</b></span>
+                      <br />
+                      &diams; {resumeData.rp3.d1}
+                      <br />
+                      &diams; {resumeData.rp3.d2}
+                      <br />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1><span>Selected Academic Projects</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                <div className="row item">
+                  <div className="twelve columns">
+                    <p>
+                      <span><b>{resumeData.sap.name} </b></span> <span>&bull;</span> <em className="date">{resumeData.sap.date}</em>
+                      <br />
+                      &diams; {resumeData.sap.d1}
+                      <br />
+                      &diams; {resumeData.sap.d2}
+                      <br />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1><span>Standardized Test Scores</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                <div className="row item">
+                  <div className="twelve columns">
+                    <p>
+                      <span><b>{resumeData.sts1.name} </b></span>
+                      <br />
+                      &rarr; {resumeData.sts1.overall}
+                      <br />
+                      &rarr; {resumeData.sts1.speaking}
+                      <br />
+                      &rarr; {resumeData.sts1.listening}
+                      <br />
+                      &rarr; {resumeData.sts1.writing}
+                      <br />
+                      &rarr; {resumeData.sts1.reading}
+                      <br />
+                    </p>
+                    <p>
+                      <span><b>{resumeData.sts2.name} </b></span>
+                      <br />
+                      &rarr; {resumeData.sts2.total}
+                      <br />
+                      &rarr; {resumeData.sts2.quantitative}
+                      <br />
+                      &rarr; {resumeData.sts2.verbal}
+                      <br />
+                      &rarr; {resumeData.sts2.awa}
+                      <br />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -69,32 +271,10 @@ export default class Resume extends Component {
         <div className="row skill">
 
           <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
+            <h1><span>References</span></h1>
           </div>
 
           <div className="nine columns main-col">
-
-            <p>
-              {resumeData.skillsDescription}
-            </p>
-
-            <div className="bars">
-
-              <ul className="skills">
-                {
-                  resumeData.skills && resumeData.skills.map((item) => {
-                    return (
-                      <li>
-                        <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                        </span><em>{item.skillname}</em>
-                      </li>
-                    )
-                  })
-                }
-
-              </ul>
-
-            </div>
 
           </div>
 
--- src/resumeData.js
+++ src/resumeData.js
@@ -5,6 +5,7 @@ let resumeData = {
   "linkedinId": "Your LinkedIn Id",
   "skypeid": "Your skypeid",
   "roleDescription": "Robotics Engineer and Research Associate.",
+
   "socialLinks": [
     {
       "name": "linkedin",
@@ -22,38 +23,120 @@ let resumeData = {
       "className": "fa fa-twitter"
     }
   ],
+
   "aboutme": "Passionate engineer, always motivated to learn new skills. I like to brainstorm and solve problems not only research but also for industry. As a robotics engineer, I have proficiency in hardware ranging from microcontroller-based (Arduino) to microprocessor-based (Raspberry Pi) and also software including MATLAB and python.",
   "address": "Bangladesh",
   "website": "Webisite: TODO",
+
+  "researchInterest": "Machine Learning Sensor and Robotics, Multi Agent Systems, Computer Vision.",
+
   "education": [
     {
       "UniversityName": "University of Dhaka",
       "specialization": "Bsc in Robotics and Mechatronics Engineering",
-      "YearOfPassing": "1 Jan 2016 - 31 Dec 2019",
-      "Achievements": "Some Achievements",
-      "address": "1000 Dhaka (Bangladesh)",
-      "finalGrade": "3.69/4",
-      "thesis": "REXPLORER: Rescue and Exploration Using Swarm of Robots",
-      "skills": "Robotics and embedded systems, Internet of Things, Microcontroller and PLC, Computer Vision (Pytorch,TensorFlow), Artificial Intelligence: Machine Learning, Deep Learning, Reinforcement Learning, MATLAB"
+      "YearOfPassing": "2016 - 2019",
+      "finalGrade": "3.69/4.00",
+      "cgpaLast2Years": "3.77"
     }
   ],
-  "work": [
+
+  "researchExperienceInfo": {
+    "name": "Independent University Bangladesh",
+    "mainDesc": "Research Associate, Center for Computational and Data Sciences.",
+    "address": "Dhaka, Bangladesh",
+    "date": "July 2022 - Present"
+  },
+
+  "researchExperienceProjects": [
     {
-      "CompanyName": "Bangladesh Robot Olympiad",
-      "specialization": "Mentor",
-      "MonthOfLeaving": "Jan",
-      "YearOfLeaving": "2018",
-      "Achievements": "Some Achievements"
+      "project": "Post-Stroke Rehabilation System",
+      "desc": "Developing a rehabilation aid system that allows the doctor to record exercises and compare exercises done by patients by motion capture devices (Microsoft Kinect and IMU device)."
     },
     {
-      "CompanyName": "Some Company",
-      "specialization": "Some specialization",
-      "MonthOfLeaving": "Jan",
-      "YearOfLeaving": "2018",
-      "Achievements": "Some Achievements"
+      "project": "Particle Tracking and Visualization (CERN)",
+      "desc": "Develop a 3D enviornment in Blender to visualize particle trackin data from ALICE detector of CERN."
+    },
+    {
+      "project": "Air Quality Monitoring System",
+      "desc": "Developed and deployed real time air particle monitoring devices to collect local air data (particles, temperature, etc.) to asses the air quality."
     }
   ],
+
+  "keySkills":
+  {
+    "programmingLang": "Python, C, C++, MATLAB",
+    "researchTool": "PyTorch, Tensoflow, LaTex",
+    "simulation": "SolidWorks",
+    "embaddedSystem": "Raspberry Pi, PLC, MCU",
+    "machineOperation": "3D printing, CNC machining"
+  },
+
+  "researchPublications": [
+    {
+      "name": "Classifying Eye-Tracking Data using Saliency Maps; ",
+      "desc": "Shafin Rahman; Sejuti Rahman; Omar Shahid; Md. Tahmeed Abdullah; Jubair Ahmed Sourov. (2021, January). Classifying Eye-Tracking Data Using Saliency Maps. In 2020 25th International Conference on Pattern Recognition (ICPR) (pp. 9288-9295). IEEE.",
+      "link": "https://ieeexplore.ieee.org/abstract/document/9412308"
+    },
+    {
+      "name": "Simulation of Pattern Formation of Swarm withMinimum Shape Parameters; ",
+      "desc": "Md. Tahmeed Ab-dullah; Md. Jubair Ahmed Sourov; Sejuti Rahman; Sujan Sarker; Joint 9th International Conference on Informatics, Electronics & Vision (ICIEV) and 2020 4th International Conference on Imaging, Vision & Pattern Recognition (icIVPR). IEEE, 2020.",
+      "link": "https://ieeexplore.ieee.org/abstract/document/9306556"
+    }
+  ],
+
+  "rp1":
+  {
+    "name": "[Funded] Developing an automated screening method of autistic children utilizing their visual attention.",
+    "d1": "Develop a novel method to classify autistic children from typically developed children usingeye-tracking data.",
+    "d2": "Collected data from the Institute of Paediatric Neuro disorder and Autism, BSMMU.",
+    "d3": "Analyzed data and develop a Machine Learning model to accurately classify healthy and children with autism.",
+    "d4": "Funded from Innovation Grant by the Ministry of ICT, Peoples Republic of Bangladesh."
+  },
+
+  "rp2":
+  {
+    "name": "[Funded] Rexplorer: Swarm robot based rescue operation and exploration",
+    "d1": "Multi agent system of independent robots for collaborative locomotion and exploration.",
+    "d2": "Robots driven by microcontroller were coordinated by a central controller.",
+    "d3": "Undergraduate final project to develop autonomous flock of swarm robots for exploration."
+  },
+
+  "rp3":
+  {
+    "name": "[Commercial] Autonomous Air conditioner control system",
+    "d1": "Developed an intelligent system to monitor room and detect human presence.",
+    "d2": "When no human is present, it turns off the air condition by wireless method."
+  },
+
+  "sap":
+  {
+    "name": "Hand Gesture Recognition System",
+    "date": "Jan 2019 - April 2019",
+    "d1": "Collected had gesture data by flex sensors.",
+    "d2": "Developed a machine learning model for automated detection hand gesture recognition and American Sign Language recognition."
+  },
+
+  "sts1":
+  {
+    "name": "IELTS",
+    "overall": "Overall : 7",
+    "speaking": "Speaking : 7",
+    "listening": "Listening : 7.5",
+    "writing": "Writing : 6.5",
+    "reading": "Reading : 7"
+  },
+
+  "sts2":
+  {
+    "name": "GRE",
+    "total": "Total : 311",
+    "quantitative": "Quantitative : 159",
+    "verbal": "Verbal : 152",
+    "awa": "AWA : 3.0"
+  },
+
   "skillsDescription": "Your skills here",
+
   "skills": [
     {
       "skillname": "HTML5"
@@ -65,6 +148,7 @@ let resumeData = {
       "skillname": "Reactjs"
     }
   ],
+
   "portfolio": [
     {
       "name": "project1",
@@ -87,6 +171,7 @@ let resumeData = {
       "imgurl": "images/portfolio/phone.jpg"
     }
   ],
+
   "testimonials": [
     {
       "description": "This is a sample testimonial",
